225d094830ca493d35599e50e971dba2fc9e505a
unknown <sohaibbinshiraz122866@gmail.com>
2026-06-14 17:24:35 +0600

    env update

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -18,39 +18,40 @@ class FrontController extends Controller
 
     $request->validate([
 
-      'full-name' => 'required|max:50|min:2',
+      'full_name' => 'required|max:50|min:2',
       'email' => 'nullable|email',
-      'parent-name' => 'required|max:50|min:5',
+      'parent_name' => 'required|max:50|min:5',
       'phone' => 'required|digits_between:11,14',
-      'payment-information' => 'required|max:100',
-      'payment-method' => 'required',
-      'transaction-id' => 'required|max:50|min:5',
+      // 'payment_information' => 'required|max:100',
+      'payment_method' => 'required',
+      'transaction_id' => 'required|max:50|min:5',
       'amount' => 'required|numeric|min:1',
 
     ], [
-      'full-name.required'           => 'Type your name',
-      'full-name.min'                => 'Name must be at least 2 characters',
-      'full-name.max'                => 'Name cannot exceed 50 characters',
+      'full_name.required'           => 'Type your name',
+      'full_name.min'                => 'Name must be at least 2 characters',
+      'full_name.max'                => 'Name cannot exceed 50 characters',
 
       'email.email'                  => 'Please enter a valid email address',
 
-      'parent-name.required'         => 'Type your parent name',
-      'parent-name.min'              => 'Parent name must be at least 5 characters',
-      'parent-name.max'              => 'Parent name cannot exceed 50 characters',
+      'parent_name.required'         => 'Type your parent name',
+      'parent_name.min'              => 'Parent name must be at least 5 characters',
+      'parent_name.max'              => 'Parent name cannot exceed 50 characters',
 
       'phone.required'               => 'Type your phone number',
       'phone.digits'                 => 'Phone number must be 11 digits',
       'phone.digits_between'         => 'Phone number is not valid',
 
-      'payment-method.required'      => 'Select a payment method',
+      'payment_method.required'      => 'Select a payment method',
 
-      'transaction-id.required'      => 'Type your transaction ID',
-      'transaction-id.min'           => 'Transaction ID is too short',
-      'transaction-id.max'           => 'Transaction ID is too long',
+      'transaction_id.required'      => 'Type your transaction ID',
+      'transaction_id.min'           => 'Transaction ID is too short',
+      'transaction_id.max'           => 'Transaction ID is too long',
 
       'amount.required'              => 'Type the amount',
       'amount.numeric'               => 'Amount must be a number',
       'amount.min'                   => 'Amount must be greater than 0',
     ]);
   }
+  
 }
